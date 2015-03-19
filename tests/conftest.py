b562d0fdc2d4aa56cd8ffa7ef90ee0a42f6aa673
Marc Sherry <msherry@gmail.com>
2015-03-19 15:08:02 -0700

    one more sexp test

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,3 +4,7 @@ import pytest
 @pytest.fixture
 def addition_sexp():
     return '(+ 2 3)'
+
+@pytest.fixture
+def subtraction_sexp():
+    return '(- 3 2)'
--- tests/test_pylisp.py
+++ tests/test_pylisp.py
@@ -5,3 +5,6 @@ class TestEval(object):
 
     def test_tokenize_addition(self, addition_sexp):
         assert tokenize(addition_sexp) == ['(', '+', '2', '3', ')']
+
+    def test_tokenize_subtraction(self, subtraction_sexp):
+        assert tokenize(subtraction_sexp) == ['(', '-', '3', '2', ')']
