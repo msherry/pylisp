b1b218c0eec899a682729f02f3e690fbcf736929
Marc Sherry <msherry@gmail.com>
2015-03-19 15:55:24 -0700

    readline history, start adding function support

--- pylisp/pylisp.py
+++ pylisp/pylisp.py
@@ -2,13 +2,25 @@
 
 from __future__ import unicode_literals
 
+import operator
 import readline
 
+# TODO: http://pymotw.com/2/readline/
+# Use readline for completing function names
 readline.parse_and_bind('tab: complete')
 readline.parse_and_bind('set editing-mode emacs')
 
+
 RED = '\x1b[31m{}\x1b[0m'
 GREEN = '\x1b[32m{}\x1b[0m'
+
+FUNCTIONS = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.div,
+    '^': operator.pow,
+}
 
 def red(s):
     return RED.format(s)
@@ -19,17 +31,31 @@ def green(s):
 def tokenize(chars):
     return chars.replace('(', ' ( ').replace(')', ' ) ').split()
 
+def get_function(func):
+    # TODO: support for environments
+    return FUNCTIONS.get(func)
+
 def l_eval(expr):
-    for tok in tokenize(expr):
-        print tok
+    tokens = tokenize(expr)
+    for i, tok in enumerate(tokens):
+        if tok == '(':
+            func = get_function(tokens[i + 1])
+            if func:
+
+                pass
+        elif tok == ')':
+            pass
 
 def read_loop():
+    try:
+        readline.read_history_file('.pylisp_history')
+    except IOError:
+        pass
     count = 1
     while True:
         try:
             expr = raw_input(red('[{}]'.format(count)) + ' > ')
         except EOFError:
-            print
             break
         if not expr:
             continue
@@ -37,6 +63,8 @@ def read_loop():
         print green('[{}]'.format(count)) + ' {}'.format(ret)
         print
         count += 1
+    print
+    readline.write_history_file('.pylisp_history')
 
 
 if __name__ == '__main__':
