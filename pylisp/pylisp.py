7e1fdaf30c86be2261971acbff111f6b82a748c0
Marc Sherry <msherry@gmail.com>
2015-04-02 16:30:54 -0700

    separate repl module

--- pylisp/pylisp.py
+++ pylisp/pylisp.py
@@ -2,16 +2,9 @@
 
 from __future__ import unicode_literals
 
-import readline
-
 from environments import Environment, global_env
-from utils import Colors, parens_balanced
-
-
-# TODO: http://pymotw.com/2/readline/
-# Use readline for completing function names
-readline.parse_and_bind('tab: complete')
-readline.parse_and_bind('set editing-mode emacs')
+from utils import Colors
+from repl import read_loop
 
 
 class Procedure(object):
@@ -163,49 +156,6 @@ def l_eval(expr, env=global_env):
             # Lisp!
             ret = None
         return ret
-
-
-def read_loop():
-    try:
-        readline.read_history_file('.pylisp_history')
-    except IOError:
-        pass
-    count = 1
-    complete_expr = []
-    indent = 0
-    while True:
-        try:
-            prompt = ((Colors.red('[{}]'.format(count)) + ' > ') if not
-                      complete_expr else '...   ' + '  ' * indent)
-            try:
-                expr = raw_input(prompt)
-            except EOFError:
-                break
-            if not expr:
-                continue
-            # Complete sexp, or multi-line entry?
-            complete_expr.append(expr)
-            balanced, indent = parens_balanced(complete_expr)
-            if balanced:
-                # Complete sexp, try to eval
-                try:
-                    expr = ' '.join(complete_expr)
-                    complete_expr = []
-                    ret = l_eval(parse(expr), global_env)
-                except Exception, e:                    # pylint: disable=W0703
-                    print 'Error: {}'.format(e)
-                    continue
-                print Colors.green('[{}]'.format(count)) + ' {}'.format(ret)
-                print
-                count += 1
-            else:
-                # Incomplete sexp, still building
-                continue
-        except KeyboardInterrupt:
-            print 'KeyboardInterrupt'
-            continue
-    print
-    readline.write_history_file('.pylisp_history')
 
 
 if __name__ == '__main__':
---
+++ pylisp/repl.py
@@ -0,0 +1,55 @@
+from __future__ import unicode_literals
+
+import readline
+
+from utils import Colors, parens_balanced
+
+
+# TODO: http://pymotw.com/2/readline/
+# Use readline for completing function names
+readline.parse_and_bind('tab: complete')
+readline.parse_and_bind('set editing-mode emacs')
+
+
+def read_loop():
+    from pylisp import l_eval, parse, global_env
+    try:
+        readline.read_history_file('.pylisp_history')
+    except IOError:
+        pass
+    count = 1
+    complete_expr = []
+    indent = 0
+    while True:
+        try:
+            prompt = ((Colors.red('[{}]'.format(count)) + ' > ') if not
+                      complete_expr else '...   ' + '  ' * indent)
+            try:
+                expr = raw_input(prompt)
+            except EOFError:
+                break
+            if not expr:
+                continue
+            # Complete sexp, or multi-line entry?
+            complete_expr.append(expr)
+            balanced, indent = parens_balanced(complete_expr)
+            if balanced:
+                # Complete sexp, try to eval
+                try:
+                    expr = ' '.join(complete_expr)
+                    complete_expr = []
+                    ret = l_eval(parse(expr), global_env)
+                except Exception, e:                    # pylint: disable=W0703
+                    print 'Error: {}'.format(e)
+                    continue
+                print Colors.green('[{}]'.format(count)) + ' {}'.format(ret)
+                print
+                count += 1
+            else:
+                # Incomplete sexp, still building
+                continue
+        except KeyboardInterrupt:
+            print 'KeyboardInterrupt'
+            continue
+    print
+    readline.write_history_file('.pylisp_history')
