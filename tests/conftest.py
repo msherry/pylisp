8bebfb936a7543c80c8cd0c7e0c992267e227a7a
Marc Sherry <msherry@gmail.com>
2015-04-02 14:34:05 -0700

    pep8

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,21 +5,26 @@ import pytest
 def addition_sexp():
     return '(+ 2 3)'
 
+
 @pytest.fixture
 def subtraction_sexp():
     return '(- 3 2)'
+
 
 @pytest.fixture
 def true_sexp():
     return '(= 7 7)'
 
+
 @pytest.fixture
 def false_sexp():
     return '(= 8 7)'
 
+
 @pytest.fixture
 def factorial_sexp():
     return '(define fact (lambda (x) (if (<= x 2) x (* x (fact (- x 1))))))'
+
 
 @pytest.fixture
 def fibonacci_sexp():
