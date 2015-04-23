fd316fab803f0b1d220627375330baa097534820
Marc Sherry <msherry@gmail.com>
2015-04-22 23:43:19 -0700

    fix broken fixtures, new memoized fib fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,7 +26,7 @@ def factorial_sexp():
     # fact is a default proc, so we define an identical fact2
     return '''(define fact2 (lambda (x)
                 (if (< x 2) x
-                  (* x (fact (- x 1))))))'''
+                  (* x (fact2 (- x 1))))))'''
 
 
 @pytest.fixture
@@ -34,7 +34,18 @@ def fibonacci_sexp():
     # fib is a default proc, so we define an identical fib2
     return '''(define fib2 (lambda (x)
                 (if (< x 2) x
-                   (+ (fib (- x 1)) (fib (- x 2))))))'''
+                   (+ (fib2 (- x 1)) (fib2 (- x 2))))))'''
+
+
+@pytest.fixture
+def memoized_fib_sexp():
+    return '''(let ((memo (make-hash-table)))
+                (define memo-fib (lambda (x)
+                  (if (gethash x memo)
+                    (gethash x memo)
+                    (set (gethash x memo)
+                      (if (< x 2) x
+                        (+ (memo-fib (- x 1)) (fib (- x 2)))))))))'''
 
 
 def memoize_sexp():
